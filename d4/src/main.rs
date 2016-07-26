ad54a3125439b28eb3ad909981b6de501ffe7cdd
Alex Eckhart <eckhartalex@gmail.com>
2016-07-25 19:12:37 -0600

    Merge day 4's part 1 and 2.

--- d4/src/main.rs
+++ d4/src/main.rs
@@ -5,6 +5,7 @@ use crypto::digest::Digest;
 fn main() {
     let key = "ckczppom";
     let mut padding = 1;
+    let mut five_zero_found = false;
     let mut md5 = md5::Md5::new();
     loop {
         md5.reset();
@@ -13,10 +14,14 @@ fn main() {
         let mut output: [u8; 16] = [0; 16];
         md5.input(&input_bytes);
         md5.result(&mut output);
+        if output[0] == 0 && output[1] == 0 && output[2] & 0b11110000 == 0  && !five_zero_found {
+            println!("The padding that makes {} have 5 zeros is {}", key, padding);
+            five_zero_found = true;
+        }
         if output[0] == 0 && output[1] == 0 && output[2] == 0 {
             break;
         }
         padding += 1;
     }
-    println!("{}", padding);
+    println!("The padding that makes {} have 6 zeros is {}", key, padding);
 }
